1eed5d14ec49fbb99394ab3e7b4ddf33dd7fd70b
Markus Karlsson <markus.karlsson.liu@gmail.com>
2022-09-18 15:49:08 +0200

    Updated shooting

--- source/gameplay/world/units/Player.cs
+++ source/gameplay/world/units/Player.cs
@@ -4,9 +4,17 @@ namespace topdownShooter
 {
     public class Player : GameObject {
         private float moveVel = 3.0F;
+        private float canShootTimerMax = 20f;
+        private float canShootTimer;
+        private float ammoMax = 3;
+        private float ammo;
+        private float ammoTimerMax = 120f;
+        private float ammoTimer;
 
         public Player(string path, Vector2 pos) : base(path, pos) {
-
+            canShootTimer = 0;
+            ammo = ammoMax;
+            ammoTimer = ammoTimerMax;
         }
 
         public override void Update() {
@@ -31,9 +39,28 @@ namespace topdownShooter
 
             rot = Globals.RotateTowards(pos, new Vector2(Globals.mouse.newMousePos.X, Globals.mouse.newMousePos.Y));
 
-            if (Globals.mouse.LeftClick()) {
-                Vector2 dir = Globals.mouse.newMousePos - pos;
-                GameGlobals.PassProjectile(new PlayerProjectile(pos, this, dir));
+            if (canShootTimer > 0) {
+                canShootTimer -= 1f;
+            }
+
+            if (ammo <= 0) {
+                if (ammoTimer > 0) {
+                    ammoTimer -= 1f;
+
+                    if (ammoTimer <= 0) {
+                        ammoTimer = ammoTimerMax;
+                        ammo = ammoMax;
+                    }
+                }
+            }
+
+            if (Globals.mouse.LeftClickHold()) {
+                if (canShootTimer <= 0 && ammo > 0) {
+                    Vector2 dir = Globals.mouse.newMousePos - pos;
+                    GameGlobals.PassProjectile(new PlayerProjectile(pos, this, dir));
+                    canShootTimer = canShootTimerMax;
+                    ammo -= 1;
+                }
             }
 
             base.Update();
