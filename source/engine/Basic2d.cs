44c91889e82028b74b899e40bd47fe584a250732
Markus Karlsson <markus.karlsson.liu@gmail.com>
2022-09-18 16:57:58 +0200

    Added alpha to basic2d

--- source/engine/Basic2d.cs
+++ source/engine/Basic2d.cs
@@ -4,11 +4,13 @@ using Microsoft.Xna.Framework.Graphics;
 namespace topdownShooter {
     public class Basic2d {
         public float rot;
+        public float alpha;
         public Vector2 pos, size, scale;
         public Texture2D texture;
 
         public Basic2d(string path, Vector2 pos) {
             this.pos = pos;
+            alpha = 1f;
             scale = new Vector2(1, 1);
             
             try {
@@ -26,11 +28,11 @@ namespace topdownShooter {
         }
 
         public virtual void Draw(Vector2 offset) {
-            Globals.spriteBatch.Draw(texture, new Rectangle((int)(pos.X + offset.X), (int)(pos.Y + offset.Y), (int)(size.X*scale.X), (int)(size.Y*scale.Y)), null, Color.White, rot, new Vector2(texture.Bounds.Width/2, texture.Bounds.Height/2), new SpriteEffects(), 0);
+            Globals.spriteBatch.Draw(texture, new Rectangle((int)(pos.X + offset.X), (int)(pos.Y + offset.Y), (int)(size.X*scale.X), (int)(size.Y*scale.Y)), null, Color.White*alpha, rot, new Vector2(texture.Bounds.Width/2, texture.Bounds.Height/2), new SpriteEffects(), 0);
         }
 
         public virtual void Draw(Vector2 offset, Vector2 origin) {
-            Globals.spriteBatch.Draw(texture, new Rectangle((int)(pos.X + offset.X), (int)(pos.Y + offset.Y), (int)(size.X*scale.X), (int)(size.Y*scale.Y)), null, Color.White, rot, new Vector2(origin.X, origin.Y), new SpriteEffects(), 0);
+            Globals.spriteBatch.Draw(texture, new Rectangle((int)(pos.X + offset.X), (int)(pos.Y + offset.Y), (int)(size.X*scale.X), (int)(size.Y*scale.Y)), null, Color.White*alpha, rot, new Vector2(origin.X, origin.Y), new SpriteEffects(), 0);
         }
     }
 }
