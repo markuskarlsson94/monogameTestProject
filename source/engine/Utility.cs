2be99cc59fcf89ceae550fb2ad138d91dc4bc64c
Markus Karlsson <markus.karlsson.liu@gmail.com>
2022-09-20 21:35:04 +0200

    Added utility functions

--- source/engine/Utility.cs
+++ source/engine/Utility.cs
@@ -1,7 +1,20 @@
+using System;
+using Microsoft.Xna.Framework;
+
 namespace topdownShooter {
     static class Utility {
         public static void Print(string str) {
             System.Console.WriteLine(str);
+        }
+
+        public static double RandomRange(double min, double max) {
+            Random rnd = new Random();
+            return rnd.NextDouble()*(max - min) + min;
+        }
+
+        public static Vector2 Vector2Rotated(Vector2 vec, float degrees) {
+            var rot = Matrix.CreateRotationZ(Globals.DegToRad(degrees));
+            return Vector2.Transform(vec, rot);
         }
     }
 }
