331dc0812c2e3627edf6c40f462f6e621c82c4d6
Markus Karlsson <markus.karlsson.liu@gmail.com>
2022-10-01 20:19:40 +0200

    Utilize shape classes in draw functions

--- source/engine/DrawFunctions.cs
+++ source/engine/DrawFunctions.cs
@@ -5,63 +5,58 @@ using System;
 namespace topdownShooter {
     static class DrawFunctions {
         public static void DrawLine(Vector2 p0, Vector2 p1, Color color = default(Color)) {
-            if (object.Equals(color, default(Color))) color = Color.White;
-            DrawLine(p0.X, p0.Y, p1.X, p1.Y, color);
+            Line2D line = new Line2D(p0, p1, color);
+            line.Draw();
         }
 
         public static void DrawLine(float x0, float y0, float x1, float y1, Color color = default(Color)) {
-            if (object.Equals(color, default(Color))) color = Color.White;
-
-            VertexPositionColor[] vertices = new[] {
-                new VertexPositionColor(new Vector3(x0, y0, 0), color),
-                new VertexPositionColor(new Vector3(x1, y1, 0), color)
-            };
-
-            BasicEffect basicEffect = new BasicEffect(Globals.graphicsDevice);
-            basicEffect.VertexColorEnabled = true;
-            basicEffect.World = Matrix.CreateOrthographicOffCenter(0, Globals.graphicsDevice.Viewport.Width, Globals.graphicsDevice.Viewport.Height, 0, 0, 1);
-
-            EffectTechnique effectTechnique = basicEffect.Techniques[0];
-            EffectPassCollection effectPassCollection = effectTechnique.Passes;
-            basicEffect.CurrentTechnique.Passes[0].Apply();
-            Globals.graphicsDevice.DrawUserPrimitives(PrimitiveType.LineStrip, vertices, 0, 1);
+            Line2D line = new Line2D(new Vector2(x0, y0), new Vector2(x1, y1), color);
+            line.Draw();
         }
 
         public static void DrawRectangle(Vector2 p0, Vector2 p1, bool filled, Color color = default(Color)) {
-            if (object.Equals(color, default(Color))) color = Color.White;
-            DrawRectangle(p0.X, p0.Y, p1.X, p1.Y, filled, color);
+            Rectangle2D rect = new Rectangle2D(p0, p1, filled, color);
+            rect.Draw();
         }
 
         public static void DrawRectangle(float x0, float y0, float x1, float y1, bool filled, Color color = default(Color)) {
-            if (object.Equals(color, default(Color))) color = Color.White;
-            VertexPositionColor[] vertices;
+            Rectangle2D rect = new Rectangle2D(new Vector2(x0, y0), new Vector2(x1, y1), filled, color);
+            rect.Draw();
+        }
 
-            if (filled) {
-                vertices = new[] {
-                    new VertexPositionColor(new Vector3(x0, y0, 0), color),
-                    new VertexPositionColor(new Vector3(x1, y0, 0), color),
-                    new VertexPositionColor(new Vector3(x0, y1, 0), color),
-                    new VertexPositionColor(new Vector3(x1, y1, 0), color)
-                };
-            } else {
-                vertices = new[] {
-                    new VertexPositionColor(new Vector3(x0, y0, 0), color),
-                    new VertexPositionColor(new Vector3(x1, y0, 0), color),
-                    new VertexPositionColor(new Vector3(x1, y1, 0), color),
-                    new VertexPositionColor(new Vector3(x0, y1, 0), color),
-                    new VertexPositionColor(new Vector3(x0, y0, 0), color)
-                };
-            }
+        public static void DrawCircle(Vector2 p, float radius, bool filled, Color color = default(Color), int segments = 32) {
+            Circle2D circle = new Circle2D(p, radius, filled, color, segments);
+            circle.Draw();
+        }
 
-            BasicEffect basicEffect = new BasicEffect(Globals.graphicsDevice);
-            basicEffect.VertexColorEnabled = true;
-            basicEffect.World = Matrix.CreateOrthographicOffCenter(0, Globals.graphicsDevice.Viewport.Width, Globals.graphicsDevice.Viewport.Height, 0, 0, 1);
+        public static void DrawCircle(float x, float y, float radius, bool filled, Color color = default(Color), int segments = 32) {
+            Circle2D circle = new Circle2D(new Vector2(x, y), radius, filled, color, segments);
+            circle.Draw();
+        }
 
-            EffectTechnique effectTechnique = basicEffect.Techniques[0];
-            EffectPassCollection effectPassCollection = effectTechnique.Passes;
-            basicEffect.CurrentTechnique.Passes[0].Apply();
-            PrimitiveType primType = (filled ? PrimitiveType.TriangleStrip : PrimitiveType.LineStrip);
-            Globals.graphicsDevice.DrawUserPrimitives(primType, vertices, 0, (filled ? 2 : 4));
+        public static void DrawRing(Vector2 p, float innerRadius, float outerRadius, Color color = default(Color), int segments = 32) {
+            Ring2D ring = new Ring2D(p, innerRadius, outerRadius, color, segments);
+            ring.Draw();
+        }
+
+        public static void DrawRing(float x, float y, float innerRadius, float outerRadius, Color color = default(Color), int segments = 32) {
+            Ring2D ring = new Ring2D(new Vector2(x, y), innerRadius, outerRadius, color, segments);
+            ring.Draw();
+        }
+
+        public static void DrawTriangle(Vector2 p0, Vector2 p1, Vector2 p2, bool filled, Color color = default(Color)) {
+            Triangle2D tri = new Triangle2D(p0, p1, p2, filled, color);
+            tri.Draw();
+        }
+
+        public static void DrawTriangle(float x0, float y0, float x1, float y1, float x2, float y2, bool filled, Color color = default(Color)) {
+            Triangle2D tri = new Triangle2D(new Vector2(x0, y0), new Vector2(x1, y1), new Vector2(x2, y2), filled, color);
+            tri.Draw();
+        }
+
+        public static void DrawPrimitive(PrimitiveType primitiveType, VertexPositionColor[] vertices) {
+            Primitive2D prim = new Primitive2D(primitiveType, vertices);
+            prim.Draw();
         }
     }
 
