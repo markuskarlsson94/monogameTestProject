7df6611faa8609ee77d3aa76c72a9514187475cf
Markus Karlsson <markus.karlsson.liu@gmail.com>
2022-10-14 17:39:48 +0200

    Made enemies red

--- source/gameplay/world/units/Enemy1.cs
+++ source/gameplay/world/units/Enemy1.cs
@@ -2,7 +2,7 @@ using Microsoft.Xna.Framework;
 
 namespace topdownShooter {
     public class Enemy1 : Enemy {
-        public Enemy1(Vector2 pos, Player player) : base("sprPlayer", pos, player) {
+        public Enemy1(Vector2 pos, Player player) : base("sprEnemy", pos, player) {
             collisionRadius = 10f;
             hp = 5;
         }
