b551aeb12a5ab9759004fd7b3a1450a2c68f8ed3
Markus Karlsson <markus.karlsson.liu@gmail.com>
2022-10-30 11:57:07 +0100

    Updated enemy spawn rate mechanic

--- source/gameplay/world/EnemySpawner.cs
+++ source/gameplay/world/EnemySpawner.cs
@@ -3,32 +3,44 @@ using Microsoft.Xna.Framework;
 
 namespace topdownShooter {
     public class EnemySpawner {
-        private float timerMax = 240f;
-        private float timer;
         private Player player;
         private Vector2 centerPos;
         private Random random;
+        private float spawnAccumulator;
+        private float spawnRate;
+        private float spawnRateDelta;
+
+        public float TimerMax { get; }
+        public float SpawnRate { get => spawnRate; }
 
         public EnemySpawner(Player player) {
             this.player = player;
-            timer = timerMax;
             centerPos = new Vector2(Globals.screenWidth/2, Globals.screenHeight/2);
             random = new Random();
+
+            spawnAccumulator= 0f;
+            spawnRate = 1/240f;
+            spawnRateDelta = 1/1200000f;
         }
 
         public void Update() {
             if (player.IsAlive()) {
-                timer -= 1f;
+                spawnRate += spawnRateDelta;
+                spawnAccumulator += spawnRate;
 
-                if (timer <= 0f) {
-                    timer = timerMax;
-                    Vector2 spawnVec = new Vector2(450, 0);
-
-                    float dir = random.Next(360);
-                    spawnVec = Utility.Vector2Rotated(spawnVec, dir);
-                    GameGlobals.PassEnemy(new Enemy1(centerPos + spawnVec, player));
+                while (spawnAccumulator >= 1f) {
+                    spawnAccumulator -= 1f;
+                    SpawnEnemy();
                 }
             }
+        }
+
+        private void SpawnEnemy() {
+            Vector2 spawnVec = new Vector2(500, 0);
+
+            float dir = random.Next(360);
+            spawnVec = Utility.Vector2Rotated(spawnVec, dir);
+            GameGlobals.PassEnemy(new Enemy1(centerPos + spawnVec, player));
         }
     }
 }
